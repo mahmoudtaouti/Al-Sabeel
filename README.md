abe27ededadac9501b97ae704a4ad50347323ebf
mahmoud <mahmoudtaouti00@gmail.com>
2021-10-05 19:02:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# al_sabeel
+
+Al-Sabeel is an application for digitize the transport sector
+
+concept by taouticc
+start : Tue 5Oct 19:00 2021
---
+++ android/app/src/main/java/com/taouticc/al_sabeel/MainActivity.java
@@ -0,0 +1,6 @@
+package com.taouticc.al_sabeel;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
